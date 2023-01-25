96830a9c83749f47c5ea47495f1163727e688565
lacoop6tu <burzi.andrea@gmail.com>
2023-01-24 20:13:46 -0500

    add initial staking contract and test

---
+++ src/Staking.sol
@@ -0,0 +1,209 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {IERC20} from "openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Ownable} from "openzeppelin/contracts/access/Ownable.sol";
+import {ReentrancyGuard} from "openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+// Each time the yield is updated:
+// The new yield will be available after a 28 days locking period or when a new yield is updated by admin, whichever comes first
+
+contract Staking is Ownable, ReentrancyGuard {
+    using SafeERC20 for IERC20;
+
+    error Error__Amount_zero();
+    error Error__Already_staked();
+    error Error__Lock_too_long();
+    error Error__Lock_too_short();
+    error Error__Nothing_to_withdraw();
+    error Error__Cannot_withdraw_yet();
+    error Error__Not_enough_rewards_available();
+    error Error__Nothing_to_claim();
+    error Error__Not_staked();
+    error Error__No_rewards_available();
+    error Error__Arrays_mismatch();
+
+    IERC20 public immutable token;
+
+    uint256 public constant maxLock = 365 days;
+    uint256 public constant minLock = 21 days;
+    uint256 public constant epoch = 28 days;
+
+    uint256 public availableRewards;
+
+
+    event Deposit(
+        address who,
+        uint256 amount,
+        uint256 lockedUntil
+    );
+
+    event Add(
+        address who,
+        uint256 amount
+    );
+
+    event Claim(
+        address who,
+        uint256 amount
+    );
+
+    event Withdraw(
+        address who,
+        uint256 amount
+    );
+
+    event Yield(
+        address who,
+        uint256 availableYield,
+        uint256 lockedYield
+    );
+
+    mapping(address => Info) public deposits;
+
+    struct Info {
+        uint256 amount;
+        uint256 lockedUntil;
+        uint256 yieldAvailable;
+        uint256 yieldLocked;
+        uint256 lastYieldUpdate;
+    }
+
+    constructor(address _token) {
+        token = IERC20(_token);
+    }
+
+
+    /// ======== External functions ======== ///
+
+    /// @notice Stake an amount of OMNIA token for a fixed period of time
+    /// @dev Sender has to approve this contract
+    /// @param amount Amount to lock [wei]
+    /// @param time Locking period [s] 
+    function stake(uint256 amount, uint256 time) external nonReentrant {
+        if (amount == 0) revert Error__Amount_zero();
+        if (deposits[msg.sender].amount != 0) revert Error__Already_staked();
+        if (time > 365 days) revert Error__Lock_too_long();
+        if (time < 21 days) revert Error__Lock_too_short();
+
+        deposits[msg.sender].amount = amount;
+        deposits[msg.sender].lockedUntil = block.timestamp + time;
+
+        token.safeTransferFrom(msg.sender, address(this), amount);
+        
+        emit Deposit(msg.sender, amount, block.timestamp + time);
+    }
+
+    /// @notice Add an amount of OMNIA for the same fixed period
+    /// @dev Sender has to approve this contract
+    /// @param amount Amount to add to the stake [wei]
+    function addToStake(uint256 amount) external nonReentrant {
+        if (deposits[msg.sender].amount == 0) revert Error__Not_staked();
+        if (amount == 0) revert Error__Amount_zero();
+        
+        deposits[msg.sender].amount += amount;
+
+        token.safeTransferFrom(msg.sender, address(this), amount);
+        
+        emit Add(msg.sender, amount); // used off-chain for the next yield epoch
+    }
+
+    /// @notice Claim all Available yield, there must be enough rewards added by the admin for distributing the yield
+    function claim_yield() external nonReentrant {
+        // TODO: add restrictions
+        uint256 amount = _claim();
+
+        token.safeTransfer(msg.sender, amount);
+      
+        emit Claim(msg.sender,amount);
+    }
+
+    /// @notice Withdraw all amount staked if lock expired
+    function withdraw() external nonReentrant {
+        uint256 amount = _withdraw();
+
+        token.safeTransfer(msg.sender, amount);
+
+        emit Withdraw(msg.sender,amount);
+    }
+
+    /// @notice Withdraw all amount staked if lock expired and also claims all available yield (if enought rewards)
+    function withdraw_and_claim() external nonReentrant {
+        uint256 withdrawAmount = _withdraw();
+        uint256 yieldAmount = _claim();
+
+        token.safeTransfer(msg.sender, yieldAmount + withdrawAmount);
+
+        emit Withdraw(msg.sender,withdrawAmount);
+        emit Claim(msg.sender,yieldAmount);
+    }
+
+    
+    /// ======== Internal functions ======== ///
+
+    function _withdraw() internal returns (uint256 amount) {
+        if (deposits[msg.sender].amount == 0)
+            revert Error__Nothing_to_withdraw();
+        if (deposits[msg.sender].lockedUntil > block.timestamp)
+            revert Error__Cannot_withdraw_yet();
+
+        amount = deposits[msg.sender].amount;
+        deposits[msg.sender].amount = 0;
+        deposits[msg.sender].lockedUntil = 0;
+    }
+
+    function _claim() internal returns (uint256 amount) {
+        
+        amount = deposits[msg.sender].yieldAvailable;
+       
+        if (block.timestamp >= deposits[msg.sender].lastYieldUpdate + epoch) {
+            amount += deposits[msg.sender].yieldLocked;
+            deposits[msg.sender].yieldLocked = 0;
+        }
+
+        if (amount == 0)
+            revert Error__Nothing_to_claim();
+        if(availableRewards == 0) revert Error__No_rewards_available();
+
+         // Will throw if not enough rewards are available, so we avoid using other users balance (last chair dance)
+        if(availableRewards < amount) revert Error__Not_enough_rewards_available();
+
+        deposits[msg.sender].yieldAvailable = 0;     
+        availableRewards -= amount;
+    }
+
+    /// ======== Admin functions ======== ///
+
+    /// @notice Add locked rewards for users, unlock the previous ones if any
+    /// @dev Only contract owner can call 
+    /// @param users Users to reward
+    /// @param yields Correspoding yields
+    function updateYields(address[] calldata users, uint256[] calldata yields) external onlyOwner {
+        if(users.length != yields.length) revert Error__Arrays_mismatch();
+        for (uint i=0; i < users.length;i++){
+            deposits[users[i]].yieldAvailable += deposits[users[i]].yieldLocked;
+            deposits[users[i]].yieldLocked = yields[i];
+            deposits[users[i]].lastYieldUpdate = block.timestamp;
+            emit Yield(users[i],deposits[users[i]].yieldAvailable, yields[i]);
+        }
+    }
+
+    /// @notice Add OMNIA rewards for users
+    /// @dev Only contract owner can call 
+    /// @param amount Rewards(yields) to be distributed
+    function addRewards(uint256 amount) external onlyOwner {
+        token.safeTransferFrom(msg.sender, address(this), amount);
+        availableRewards += amount;
+    }
+
+    /// @notice Withdraw any OMNIA rewards in case of a shutdown (not users stakes)
+    /// @dev Only contract owner can call 
+    function withdrawRewards() external onlyOwner {
+        if (availableRewards > 0) {
+            uint256 amount = availableRewards;
+            availableRewards = 0;
+            token.safeTransfer(msg.sender, amount);
+        }
+    }
+}
---
+++ test/Staking.t.sol
@@ -0,0 +1,334 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import {Staking} from "../src/Staking.sol";
+import {ERC20} from "openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Omnia is ERC20("Omnia", "OMN") {
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
+
+contract StakingTest is Test {
+    Staking public staking;
+    Omnia public omnia;
+
+    address me = address(this);
+    address staker1 = address(1);
+    address staker2 = address(2);
+    address staker3 = address(3);
+
+    uint256 initialAmount = 1000 ether;
+    uint256 epoch = 28 days;
+
+    address[] users;
+    uint256[] yields;
+
+    function setUp() public {
+        omnia = new Omnia();
+        staking = new Staking(address(omnia));
+        omnia.mint(me, initialAmount);
+        omnia.mint(staker1, initialAmount);
+        omnia.mint(staker2, initialAmount);
+        omnia.mint(staker3, initialAmount);
+        omnia.approve(address(staking), initialAmount);
+        vm.prank(staker1);
+        omnia.approve(address(staking), initialAmount);
+        vm.prank(staker2);
+        omnia.approve(address(staking), initialAmount);
+        vm.prank(staker3);
+        omnia.approve(address(staking), initialAmount);
+    }
+
+    function test_check_initialization() public {
+        assertEq(staking.maxLock(), 365 days);
+        assertEq(staking.minLock(), 21 days);
+        assertEq(staking.epoch(), 28 days);
+        assertEq(address(staking.token()), address(omnia));
+        assertEq(staking.availableRewards(), 0);
+        assertEq(omnia.balanceOf(me), initialAmount);
+        assertEq(omnia.balanceOf(staker1), initialAmount);
+    }
+
+    function test_1_user_flow() public {
+        // Adding rewards (admin)
+        staking.addRewards(100 ether);
+
+        // User stake
+        uint256 stakeAmount = 100 ether;
+
+        vm.prank(staker1);
+        staking.stake(stakeAmount, 180 days);
+
+        (uint256 amount, , , , ) = staking.deposits(staker1);
+        assertEq(amount, stakeAmount);
+        
+
+        users.push(staker1);
+        uint256 yield = 10 ether;
+   
+        yields.push(yield);
+        // Update yield (admin)
+        staking.updateYields(users, yields);
+
+        vm.warp(block.timestamp + epoch);
+
+        // Staker claims yield
+        vm.prank(staker1);
+        staking.claim_yield();
+        (amount, , , , ) = staking.deposits(staker1);
+        assertEq(amount, stakeAmount);
+        assertEq(omnia.balanceOf(staker1), initialAmount - stakeAmount + yield);
+
+        // we update twice before user collects yield
+        staking.updateYields(users, yields);
+
+        vm.warp(block.timestamp + 30 days);
+
+        staking.updateYields(users, yields);
+
+        vm.warp(block.timestamp + 30 days);
+
+        vm.prank(staker1);
+        staking.claim_yield();
+        assertEq(
+            omnia.balanceOf(staker1),
+            initialAmount - stakeAmount + 3 * yield
+        );
+
+        // user add more stake
+        vm.prank(staker1);
+        staking.addToStake(stakeAmount);
+
+        // we add more yield
+        staking.updateYields(users, yields);
+
+        // move after lock
+        vm.warp(block.timestamp + 120 days);
+
+        // user can now withdraw and leave some unleft yield to claim;
+        vm.prank(staker1);
+        staking.withdraw();
+        assertEq(omnia.balanceOf(staker1), initialAmount + 3 * yield);
+
+        // user can still claim available yield after withdrawing
+        vm.warp(block.timestamp + 30 days);
+
+        vm.prank(staker1);
+        staking.claim_yield();
+        assertEq(omnia.balanceOf(staker1), initialAmount + 4 * yield);
+    }
+
+    function test_3_users_flow() public {
+          // Adding rewards (admin)
+        staking.addRewards(100 ether);
+
+        // Users stake
+        uint256 stakeAmount1 = 100 ether;
+        uint256 stakeAmount2 = 50 ether;
+        uint256 stakeAmount3 = 30 ether;
+
+        vm.prank(staker1);
+        staking.stake(stakeAmount1, 90 days);
+        vm.prank(staker2);
+        staking.stake(stakeAmount2, 120 days);
+        vm.prank(staker3);
+        staking.stake(stakeAmount3, 60 days);
+
+        (uint256 amount, , , , ) = staking.deposits(staker1);
+        assertEq(amount, stakeAmount1);
+        (amount, , , , ) = staking.deposits(staker2);
+        assertEq(amount, stakeAmount2);
+        (amount, , , , ) = staking.deposits(staker3);
+        assertEq(amount, stakeAmount3);
+
+        users.push(staker1);
+        users.push(staker2);
+        users.push(staker3);
+
+        uint256 yield1 = 10 ether;
+        uint256 yield2 = 7 ether;
+        uint256 yield3 = 5 ether;
+
+        yields.push(yield1);
+        yields.push(yield2);
+        yields.push(yield3);
+        staking.updateYields(users, yields);
+        
+        vm.warp(block.timestamp + epoch);
+
+        // User1 claims all yield available
+        vm.prank(staker1);
+        staking.claim_yield();
+        (, , uint256 availableYield,uint256 lockedYield , ) = staking.deposits(staker1);
+        assertEq(availableYield,0);
+        assertEq(lockedYield,0);
+
+        // User1 has now the initialAmount - amount stake + all yield
+        assertEq(omnia.balanceOf(staker1),initialAmount - stakeAmount1 + yield1);
+
+        staking.updateYields(users, yields);
+        vm.warp(block.timestamp + 32 days);
+
+        staking.updateYields(users, yields);
+        
+        // user3 exits and withdraws stake and available yield
+        vm.prank(staker3);
+        staking.withdraw_and_claim();
+        uint256 lastTimeUpdate;
+        uint256 lockedUntil;
+        (amount, lockedUntil,availableYield,lockedYield, lastTimeUpdate) = staking.deposits(staker3);
+        assertEq(amount,0);
+        assertEq(lockedUntil,0);
+        assertEq(availableYield,0); 
+        assertGt(lockedYield,0); // There's some locked yield waited to be unlocked
+        assertGt(lastTimeUpdate,0);
+
+        // User3 has now the initialAmount + the available yield 
+        assertEq(omnia.balanceOf(staker3),initialAmount + 2 * yield3);
+
+        // User3 re-stake for some time
+        vm.prank(staker3);
+        staking.stake(stakeAmount3,30 days);
+
+        staking.updateYields(users, yields);
+
+        vm.warp(block.timestamp + 30 days);
+
+        // User3 exits and withdraws stake and available yield (ALL yield is available in this case)
+        vm.prank(staker3);
+        staking.withdraw_and_claim();
+        (amount, lockedUntil,availableYield,lockedYield, lastTimeUpdate) = staking.deposits(staker3);
+        assertEq(amount,0);
+        assertEq(lockedUntil,0);
+        assertEq(availableYield,0); 
+        assertEq(lockedYield,0); // There's no locked yield
+        assertGt(lastTimeUpdate,0); 
+
+        // User3 has now the initialAmount + all yield
+        assertEq(omnia.balanceOf(staker3),initialAmount + 4 * yield3);
+
+        // User1 withdraws only the stake
+        vm.prank(staker1);
+        staking.withdraw();
+
+        (amount,lockedUntil,availableYield,lockedYield,) = staking.deposits(staker1);
+        assertEq(amount,0);
+        assertEq(lockedUntil,0);
+        assertGt(availableYield,0); // There's available yield
+        assertGt(lockedYield,0); // There's locked yield which by now is free to be withdrawn
+
+        // User1 got back is stake and has only the intiial yield claimed before
+        assertEq(omnia.balanceOf(staker1),initialAmount + yield1);
+
+        vm.prank(staker1);
+        staking.claim_yield();
+        (,,availableYield,lockedYield,) = staking.deposits(staker1);
+        assertEq(availableYield,0); 
+        assertEq(lockedYield,0); 
+
+        // User1 collected all available yield
+        assertEq(omnia.balanceOf(staker1),initialAmount + 4 *yield1);
+
+        vm.warp(block.timestamp + 90 days);
+
+        vm.prank(staker2);
+        staking.withdraw_and_claim();
+
+        // User2 collected all together
+        assertEq(omnia.balanceOf(staker2),initialAmount + 4 *yield2);
+
+
+    }
+
+    function test_errors() public {
+        vm.startPrank(staker1);
+
+        // Amount Zero
+        vm.expectRevert(Staking.Error__Amount_zero.selector);
+        staking.stake(0, 365 days);
+
+        // No rewards available
+        vm.expectRevert(Staking.Error__Nothing_to_claim.selector);
+        staking.claim_yield();
+
+        // Stake more than 365 days
+        vm.expectRevert(Staking.Error__Lock_too_long.selector);
+        staking.stake(10, 366 days);
+
+        // Stake less than 21 days
+        vm.expectRevert(Staking.Error__Lock_too_short.selector);
+        staking.stake(10 ether, 20 days);
+
+        // Attemp add more stake without having a live stake
+        vm.expectRevert(Staking.Error__Not_staked.selector);
+        staking.addToStake(10 ether);
+
+        // Attemp withdraw without staking
+        vm.expectRevert(Staking.Error__Nothing_to_withdraw.selector);
+        staking.withdraw();
+
+        // USER STAKES
+        staking.stake(100 ether, 180 days);
+
+        vm.expectRevert(Staking.Error__Already_staked.selector);
+        staking.stake(30 ether, 60 days);
+
+        vm.stopPrank();
+
+        // 50 OMNIA rewards for staker1
+        users.push(staker1);
+        yields.push(50 ether);
+
+        // Admin set yields
+        staking.updateYields(users, yields);
+
+        vm.startPrank(staker1);
+        // User has nothing to claim yet (28 days have not passed)
+        vm.expectRevert(Staking.Error__Nothing_to_claim.selector);
+        staking.claim_yield();
+
+        // Cannot withdraw yet
+        vm.expectRevert(Staking.Error__Cannot_withdraw_yet.selector);
+        staking.withdraw();
+
+        // Cannot use withdraw and claim yet
+        vm.expectRevert(Staking.Error__Cannot_withdraw_yet.selector);
+        staking.withdraw_and_claim();
+
+        // We advanced when they are unlocked
+        vm.warp(block.timestamp + 28 days);
+
+        // User cannot get reward because there's no reward available
+        vm.expectRevert(Staking.Error__No_rewards_available.selector);
+        staking.claim_yield();
+
+        vm.stopPrank();
+
+        // we add some rewards as admin
+        staking.addRewards(40 ether);
+
+        // User cannot get reward because there's not enough reward available
+        vm.expectRevert(Staking.Error__Not_enough_rewards_available.selector);
+        vm.prank(staker1);
+        staking.claim_yield();
+
+        // we add enough rewards as admin
+        staking.addRewards(40 ether);
+
+        vm.startPrank(staker1);
+        staking.claim_yield();
+
+        assertEq(staking.availableRewards(), 30 ether);
+        assertEq(omnia.balanceOf(staker1), 950 ether);
+
+        // after enough time user can withdraw
+        vm.warp(block.timestamp + 180 days);
+        staking.withdraw();
+
+        assertEq(omnia.balanceOf(staker1), initialAmount + 50 ether);
+    }
+}
